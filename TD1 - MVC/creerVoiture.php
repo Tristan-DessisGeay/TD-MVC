9a343783103f10570fd86f17d3b255dd89195e7b
Tristan-DessisGeay <107466321+Tristan-DessisGeay@users.noreply.github.com>
2022-10-14 18:50:33 +0200

    Delete TD1 - MVC directory

--- TD1 - MVC/creerVoiture.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if(count($_GET)>0) {
-        require_once 'Voiture.php';
-        $voiture = new Voiture($_GET['marque'], $_GET['couleur'], $_GET['immatriculation']);
-        $voiture->afficher();
-    }
-?>
