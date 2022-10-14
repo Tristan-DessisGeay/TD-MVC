be769615c4989474cf4146cae0b486516426afe7
Tristan-DessisGeay <107466321+Tristan-DessisGeay@users.noreply.github.com>
2022-10-14 18:50:53 +0200

    Delete TD3 - MVC directory

--- TD3 - MVC/lireVoiture.php
+++
@@ -1,5 +0,0 @@
-<?php
-    require_once "Voiture.php"; 
-    $v1 = new Voiture("BO-67-AS", "Bugatti", "Bleu");
-    $v1->save();
-?>
--- TD3 - MVC/testFindUtil.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if(count($_GET)>0){
-        require_once "Trajet.php";
-        $passagers = Trajet::findPassagers($_GET['trajet_id']);
-        foreach($passagers as $p){
-            $p->afficher();
-        }
-    }
-?>
